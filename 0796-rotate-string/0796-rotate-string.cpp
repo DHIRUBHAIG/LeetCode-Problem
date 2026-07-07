fb566e04f5df1b776622dcad996855a66c8c60f2
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-07-07 11:56:32 +0530

    Time: 0 ms (100%), Space: 8.1 MB (53.19%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        string temp=s+s;
+        if(s.length()!=goal.length()){
+            return false;
+        }
+
+        if(temp.find(goal)!=string::npos){//Checks whether the substring goal exists anywhere in the string.
+            return true;
+        }
+
+        return false;
+    }
+};
