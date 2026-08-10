8905f18ac7d65b9e802b94af0aa796ec09d2b212
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-08-11 01:13:27 +0530

    Time: 0 ms (100%), Space: 15.1 MB (22.02%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,33 +12,40 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
-        if(root==NULL) return ans;
-        queue<pair<TreeNode*,int>>q;
-        map<int,int>mpp;
-        q.push({root,0});
-        while(!q.empty()){
-            int n=q.size();
-            while((n--)){
-                auto it=q.front();
+
+        vector<int> ans;
+
+        if (root == NULL)
+            return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+
+            int n = q.size();
+
+            while (n--) {
+
+                TreeNode* node = q.front();
                 q.pop();
-                TreeNode* node=it.first;
-                int line=it.second;
-                mpp[line]=node->val;
-                if(node->left!=NULL){
-                    q.push({node->left,line+1});
+
+                // First node of the level from the right side
+                if (n == 0) {
+                    ans.push_back(node->val);
                 }
-                if(node->right!=NULL){
-                    q.push({node->right,line+1});
+
+                // Push left first, then right
+                if (node->left != NULL) {
+                    q.push(node->left);
+                }
+
+                if (node->right != NULL) {
+                    q.push(node->right);
                 }
             }
-
-       }
-        for(auto it:mpp){
-            ans.push_back(it.second);
         }
+
         return ans;
-
-
     }
 };
