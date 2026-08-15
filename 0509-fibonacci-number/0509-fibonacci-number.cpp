310528c596ec8fe543aff3fdbfe993647affb5bb
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-08-15 12:27:05 +0530

    Time: 3 ms (64.79%), Space: 8.3 MB (11.33%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-int fibo(int n,vector<int>&dp){
-    // base case;
-    if(n<=1) return n;
-    //check if exist in dp arrays ->then return
-    if(dp[n]!=-1) return dp[n];
-    // store in dp;
+// Tabulation ->Bottom-up Approach;
 
-    dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-    // return dp arrays;
-    return dp[n];
-}
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-       return fibo(n,dp);
+         vector<int>dp(n+1);
+        if(n <= 1)
+            return n;
+       
+         dp[0]=0;
+         dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
