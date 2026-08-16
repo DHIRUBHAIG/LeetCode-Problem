5cebe18df4bc3588fe4bee6952f58e076d53adcb
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-08-17 01:27:21 +0530

    Time: 0 ms (100%), Space: 10.6 MB (65.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-int solve(vector<int>&nums,int n,vector<int>&dp){
-    if(n==0) return nums[0];
-    
-    if(n<0) return 0;
-    if(dp[n]!=-1) return dp[n];
-
-    int incl=solve(nums,n-2,dp)+nums[n];
-    int excl=solve(nums,n-1,dp)+0;
-    dp[n]= max(incl,excl);
-    return dp[n];
-}
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return solve(nums,n-1,dp);
+        int n = nums.size();
+
+        // Edge case
+        if (n == 1)
+            return nums[0];
+
+        // DP array
+        vector<int> dp(n, 0);
+
+        // Base cases
+        // heving two size of arrays;
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        // Build DP table
+        for (int i = 2; i < n; i++) {
+
+            int incl = dp[i - 2] + nums[i];
+            int excl = dp[i - 1];
+
+            dp[i] = max(incl, excl);
+        }
+
+        return dp[n - 1];
     }
-};
+}; 
