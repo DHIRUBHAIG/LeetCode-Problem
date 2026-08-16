dac7cabcbffa8cb75bc8877c21da0740b744c59e
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-08-17 01:34:46 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.8%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,23 +7,25 @@ public:
         if (n == 1)
             return nums[0];
 
-        // DP array
-        vector<int> dp(n, 0);
+        
 
         // Base cases
         // heving two size of arrays;
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+       int prev2 = nums[0];
+       int prev1 = max(nums[0], nums[1]);
 
         // Build DP table
         for (int i = 2; i < n; i++) {
 
-            int incl = dp[i - 2] + nums[i];
-            int excl = dp[i - 1];
+            int incl = prev2 + nums[i];
+            int excl = prev1;
 
-            dp[i] = max(incl, excl);
+           int curr = max(incl, excl);
+            prev2=prev1;
+            prev1=curr;
+
         }
 
-        return dp[n - 1];
+        return prev1;
     }
 }; 
