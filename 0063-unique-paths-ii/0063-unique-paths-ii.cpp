842fdc768a48c4c292cc408e6cbe605f532a32b9
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-08-17 19:54:11 +0530

    Time: 0 ms (100%), Space: 12.1 MB (6.62%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
-int solve(vector<vector<int>>& obstacleGrid,int m,int n,vector<vector<int>>&dp){
+int solve(vector<vector<int>>& obstacleGrid,int m,int n){
+     vector<vector<int>>dp(m,vector<int>(n,-1));
     if(obstacleGrid[0][0]==1) return 0; 
-    if(m==0 && n==0){
-        return 1;
-    }
-    if(dp[m][n]!=-1) return dp[m][n];
+    dp[0][0]=1;
+    // if(dp[m][n]!=-1) return dp[m][n];
 
-    if(obstacleGrid[m][n]==1) return 0;
-    int down=0;
-    int right=0;
+    
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(obstacleGrid[i][j]==1){
+                dp[i][j] = 0;
+                continue;
+            }
+
+            if(i==0 && j==0){
+                dp[0][0]=1;
+                continue;
+            }
+            int down=0;
+             int right=0;
    
 
-    if(m==0){
-        //only right;
-        right=solve(obstacleGrid,m,n-1,dp);
+    if(i>0){
+        //only down;
+        right=dp[i-1][j];
     }
-    else if(n==0){
-        // only down;
-        down=solve(obstacleGrid,m-1,n,dp);
+     if(j>0){
+        // only right;
+        down=dp[i][j-1];
         
     }
-    else{
-        // both right and down;
-        right=solve(obstacleGrid,m,n-1,dp);
-        down=solve(obstacleGrid,m-1,n,dp);
+    
+    dp[i][j]= right+down;
+
+        }
     }
-    dp[m][n]= right+down;
-    return dp[m][n];
+    
+    return dp[m-1][n-1];
 }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
         
-        return solve(obstacleGrid,m-1,n-1,dp);
+        return solve(obstacleGrid,m,n);
     }
 };
