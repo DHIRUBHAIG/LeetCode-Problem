d922598e01449ccad9326315ea7a1bd3999deb75
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-08-18 19:05:34 +0530

    Time: 0 ms (100%), Space: 16.5 MB (16.7%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,31 +1,60 @@
 class Solution {
 public:
-int solve(vector<vector<int>>& grid,int m,int n,vector<vector<int>>&dp){
-    if(m==0 && n==0) return grid[0][0];
-    if(dp[m][n]!=-1) return dp[m][n];
-    int right=INT_MAX;
-    int down=INT_MAX;
+int solve(vector<vector<int>>& grid,int m,int n){
+    // if(m==0 && n==0) return grid[0][0];
+    // if(dp[m][n]!=-1) return dp[m][n];
+    // int right=INT_MAX;
+    // int down=INT_MAX;
 
-    if(m==0) {
-        right=grid[m][n]+solve(grid,m,n-1,dp);
+    // if(m==0) {
+    //     right=grid[m][n]+solve(grid,m,n-1);
 
-    }
-    else if(n==0){
-        down=grid[m][n]+solve(grid,m-1,n,dp);
-    }
-    else{
-        right=grid[m][n]+solve(grid,m,n-1,dp);
-        down=grid[m][n]+solve(grid,m-1,n,dp);
-    }
+    // }
+    // else if(n==0){
+    //     down=grid[m][n]+solve(grid,m-1,n);
+    // }
+    // else{
+    //     right=grid[m][n]+solve(grid,m,n-1);
+    //     down=grid[m][n]+solve(grid,m-1,n);
+    // }
     
 
-    dp[m][n]= min(right,down);
-    return dp[m][n];
+    // dp[m][n]= min(right,down);
+    // return dp[m][n];
+
+    vector<vector<int>>dp(m,vector<int>(n,-1));
+
+    // Base case
+    dp[0][0] = grid[0][0];
+   
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+
+            if(i == 0 && j == 0)
+                    continue;
+         int right=INT_MAX;
+    int down=INT_MAX;
+            if(i==0) {
+        right=grid[i][j]+dp[i][j-1];
+
+    }
+    else if(j==0){
+        down=grid[i][j]+dp[i-1][j];
+    }
+    else{
+        right=grid[i][j]+dp[i][j-1];
+        down=grid[i][j]+dp[i-1][j];
+    }
+    dp[i][j]= min(right,down);
+
+        }
+    }
+    return dp[m-1][n-1];
 }
     int minPathSum(vector<vector<int>>& grid) {
        int m=grid.size();
        int n=grid[0].size();
-       vector<vector<int>>dp(m,vector<int>(n,-1));
-       return solve(grid,m-1,n-1,dp); 
+       //vector<vector<int>>dp(m,vector<int>(n,-1));
+       return solve(grid,m,n); 
     }
 };
