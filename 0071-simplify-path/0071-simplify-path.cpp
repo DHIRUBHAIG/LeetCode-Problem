235d06cc482c27d58efb95524b12b5cf0e87b31f
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-09-01 17:47:58 +0530

    Time: 0 ms (100%), Space: 15.3 MB (14.35%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,30 @@
+// ""   → ignore
+// "."  → ignore
+// ".." → pop
+// other → push
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string>st;
+        string token="";
+        stringstream ss(path);
+        while(getline(ss,token,'/')){
+            if(token==""||token==".") continue;
+            else if(token==".." ){
+                if( !st.empty())
+                st.pop();
+            } 
+            else{
+                st.push(token);
+            }
+        }
+        if(st.empty()) return "/";
+        string result="";
+        while(!st.empty()){
+            result="/"+st.top()+result;
+            st.pop();
+        }
+        return result;
+    }
+};
