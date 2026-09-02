c8a255df49f93772313b0655c7960354d5390582
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-09-02 13:03:21 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        vector<int>ans;
+        //vector<int>ans;
         for(int i=0;i<nums1.size();i++){
         
                 if(nums1[i]%2!=0){
-                ans.push_back(nums1[i]);
+                //ans.push_back(nums1[i]);
+                continue;
             }
             else{
                 for(int j=0;j<nums1.size();j++){
                     if(i!=j){
                         if((nums1[i]-nums1[j]%2)!=0){
-                            ans.push_back(nums1[i]-nums1[j]);
+                           // ans.push_back(nums1[i]-nums1[j]);
+                           continue;
                             break;
                         }
                         return false;
