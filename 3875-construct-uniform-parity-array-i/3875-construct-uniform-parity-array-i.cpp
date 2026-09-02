6be8907eed76f3fafacb95f909a9f5983e64e84d
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-09-02 13:02:25 +0530

    Time: 0 ms (100%), Space: 30.5 MB (7.23%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -3,7 +3,7 @@ public:
     bool uniformArray(vector<int>& nums1) {
         vector<int>ans;
         for(int i=0;i<nums1.size();i++){
-        if(nums1[i]%2==0){
+        
                 if(nums1[i]%2!=0){
                 ans.push_back(nums1[i]);
             }
@@ -19,7 +19,7 @@ public:
                 }
                 
             }
-        }
+        
         }
         return true;
     }
