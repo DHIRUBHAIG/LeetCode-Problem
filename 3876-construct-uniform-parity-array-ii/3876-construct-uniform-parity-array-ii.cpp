ef1e6db79a4be904a46f65d9cf6aa7820ca29c90
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-09-03 13:35:13 +0530

    Time: 7 ms (51.05%), Space: 165.7 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& nums1) {
-        sort(nums1.begin(),nums1.end());
-       // vector<int>ans;
-        for(int i=0;i<nums1.size();i++){
-            if(nums1[0]%2==0){
-                if(nums1[i]%2==0){
-                    //ans.push_back(nums1[i]);
+// | Operation       | Result |
+// | --------------- | ------ |
+// | **Even − Even** | Even   |
+// | **Odd − Odd**   | Even   |
+// | **Even − Odd**  | Odd    |
+// | **Odd − Even**  | Odd    |
 
-                }
-                else{
-                    for(int j=0;j<i;j++){
-                        int rem=nums1[i]-nums1[j];
-                        if(rem>=1 && rem%2==0){
-                            //ans.push_back(nums1[j]);
-                            break;
-                        }
-                        else return false;
-                    }
-                }
-               
-            }
-             else{
-                    if(nums1[i]%2!=0){
-                        //ans.push_back(nums1[i]);
-                    }
-                    else{
-                        for(int j=0;j<i;j++){
-                            int rem=nums1[i]-nums1[j];
-                            if(rem>=1 && rem%2!=0){
-                                //ans.push_back(nums1[j]);
-                                break;
-                            }
-                            else return false;
-                        }
-                    }
-                }
+    bool uniformArray(vector<int>& nums1) {
+        int mn = *min_element(nums1.begin(), nums1.end());
+
+        // Minimum is odd:
+        //converatble in odd of all even
+        // every even number can become odd by subtracting mn.
+        if (mn % 2 != 0)
+            return true;
+
+        // Minimum is even:
+        // every element must already be even.
+        for (int x : nums1) {
+            if (x % 2 != 0)
+                return false;
         }
+
         return true;
     }
 };
