bf29cc9bd47f23934f77d8087d4fab1a5b0b8a21
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-09-05 00:46:58 +0530

    Time: 1 ms (12.33%), Space: 17.7 MB (30.06%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
+    int solve(vector<int>& cost, int n,vector<int>&dp) {
+        if (n == 0 || n == 1)
+            return 0;
+        if(dp[n]!=-1){
+            return dp[n];
+        } 
+
+        dp[n]= min(
+            solve(cost, n - 1,dp) + cost[n - 1],
+            solve(cost, n - 2,dp) + cost[n - 2]
+        );
+        return dp[n];
+    }
 
     int minCostClimbingStairs(vector<int>& cost) {
-
         int n = cost.size();
-
-        // prev2 = dp[i-2]
-        int prev2 = 0;
-
-        // prev1 = dp[i-1]
-        int prev1 = 0;
-
-        for (int i = 2; i <= n; i++) {
-
-            // Cost if we come from i-1.
-            int oneStep = prev1 + cost[i - 1];
-
-            // Cost if we come from i-2.
-            int twoStep = prev2 + cost[i - 2];
-
-            // Minimum cost to reach current step.
-            int curr = min(oneStep, twoStep);
-
-            // Move forward.
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        // prev1 contains the minimum cost
-        // to reach the top.
-        return prev1;
+        vector<int>dp(n+1,-1);
+        return solve(cost, n,dp);
     }
 };
