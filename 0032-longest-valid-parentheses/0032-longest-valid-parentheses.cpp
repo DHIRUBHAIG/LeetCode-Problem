e63f1f84ae77d58b96ef4c72e1d64191b937607f
DHIRU BHAI <dhirajkumarg413@gmail.com>
2026-09-04 12:30:04 +0530

    Time: 3 ms (39.95%), Space: 11.8 MB (50.13%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,47 +1,28 @@
-// Why two scans?
-
-// Because one direction cannot handle both situations.
-
-// Left → Right handles cases where ) becomes greater than (.
-// Right → Left handles cases where ( remains greater than ).
-
 class Solution {
 public:
     int longestValidParentheses(string s) {
-       int left=0;
-       int right=0;
-       int maxlen=0;
-       //from left to right;
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='('){
-            left++;
+        stack<int> st;
+        st.push(-1);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] == '(') {
+                st.push(i);
+            }
+            else {
+                st.pop();
+
+                if (st.empty()) {
+                    st.push(i);
+                }
+                else {
+                    ans = max(ans, i - st.top());
+                }
+            }
         }
-        else right++;
-        if(left==right){
-            maxlen=max(maxlen,(2*left));
-        }
-        if(right>left){
-            left=0;
-            right=0;
-        }
-    } 
-    left=0;
-    right=0;
-    
-    for(int i=s.size()-1;i>=0;i--){
-        if(s[i]=='('){
-            left++;
-        }
-        else right++;
-        if(left==right){
-            maxlen=max(maxlen,(2*left));
-        }
-        if(right<left){
-            left=0;
-            right=0;
-        }
-    }
-    return maxlen;
-    
+
+        return ans;
     }
 };
